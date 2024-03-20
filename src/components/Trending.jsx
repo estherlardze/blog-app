636f4b7593701ba251260732010b65af64388f60
estherlardze <estherlardze8@gmail.com>
2024-03-20 23:16:05 +0000

    slider working

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Link } from 'react-router-dom'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
 
 const Trending = ({ blogs }) => {
+ 
+  const scrollRef = useRef(null)
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 3
-  };
+  const scroll = (direction) => {
+    const { current } =  scrollRef;
+
+    if(direction === 'left') {
+      current.scrollLeft =- 300
+    }else if(direction === 'right'){
+      current.scrollLeft =+ 300
+    }
+  }
+
 
   console.log('blogs from trending: ', blogs)
   return (
-       <section >
+       <section className='w-[90%] mx-[5%]'>
          <h1 className='mb-3 border-b-2'>Trending Blogs</h1>
-         <div className='w-[90%] mx-[5%] overflow-scroll flex gap-4'>
+         <div className='overflow-x-scroll flex gap-4 container scroll-container' ref={scrollRef}>
          {blogs.map((blog) => (
             <article className=''>
                 {
@@ -38,6 +42,16 @@ const Trending = ({ blogs }) => {
             )}
             </article>
         ))}
+         </div>
+
+         <div className='flex justify-center items-center gap-3 mt-6'>
+            <IoIosArrowBack 
+              size={27} className='hover:bg-black/10 rounded-full p-1 cursor-pointer transition-all'
+              onClick={() => scroll('left')}/>
+            <IoIosArrowForward 
+              size={27} 
+              className='hover:bg-black/10 rounded-full p-1 cursor-pointer transition-all'
+              onClick={() => scroll('right')}/>
          </div>
     </section>
   )
